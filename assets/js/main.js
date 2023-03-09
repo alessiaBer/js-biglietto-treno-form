c29b78bbc8a2b874f9c6ecd83ad7d713ae085992
alessiaBer <alexdeyn@gmail.com>
2023-03-09 14:44:15 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+/* MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica ma usando esclusivamente due input e un bottone (non stilizzati), 
+realizziamo le specifiche scritte sopra.
+La risposta finale (o output) sarà anch’essa da scrivere in console */
